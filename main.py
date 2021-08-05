d48e18b9e6f24adf77e0a8cf4cd343f13e8d0e29
aerinchiu <88351294+aerin-c@users.noreply.github.com>
2021-08-05 20:48:31 +0000

    Completed step 2 of the lab.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # The goal of this program is to practice Python constructs
 
 def getNumber():
+  inputs = []
+  symbols = 0
+  while symbols >=0:
+    symbols = int(input("Enter a digit: "))
+    if symbols > -1:
+      inputs.append(symbols)
+    else:
+      break
+  print(sum(inputs))
 
-   symbols = input("Enter a digit: ")
-
-   number = int(symbols)
-
-   return number
-   
 def sumTwo(a,b):
 
    c = a + b
@@ -19,4 +22,5 @@ def sumTwo(a,b):
 x = sumTwo(3,5)
 
 print(x)
+getNumber()
 
